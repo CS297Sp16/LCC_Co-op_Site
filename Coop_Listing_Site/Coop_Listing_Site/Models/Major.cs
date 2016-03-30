f8caec54be95ea3cdeb197db59b2a9357b352581
Ben Middleton <pheendish@gmail.com>
2016-03-30 15:01:58 -0700

    Make Class
    Create major class

---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Major.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class Major
+    {
+        public int MajorID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
