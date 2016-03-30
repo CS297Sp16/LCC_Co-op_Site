710f68b46007b0487450468109dcdcf94681774b
gafler <griffin.gabe@gmail.com>
2016-03-30 15:48:51 -0700

    Added the student class

---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class Student : User
+    {
+        [Required]
+        [Display(Name ="L-Number")]
+        [RegularExpression("^[L]\\d{8}", ErrorMessage ="Your L-Number must contain a capitol L followed by 8 digits")]
+        public string LNumber { get; set; }
+
+        public int MajorID { get; set; }
+    }
+}
