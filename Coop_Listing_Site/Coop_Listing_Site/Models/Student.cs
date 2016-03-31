27a7e833018a940d06a91ca56372d07344e0799a
gafler <griffin.gabe@gmail.com>
2016-03-30 20:08:44 -0700

    spelling changes

--- Coop_Listing_Site/Coop_Listing_Site/Models/Student.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Student.cs
@@ -10,7 +10,7 @@ namespace Coop_Listing_Site.Models
     {
         [Required(ErrorMessage ="You must enter a valid L-Number")]
         [Display(Name ="L-Number")]
-        [RegularExpression("^[Ll]\\d{8}", ErrorMessage ="Your L-Number must contain a capitol L followed by 8 digits")]
+        [RegularExpression("^[Ll]\\d{8}", ErrorMessage ="Your L-Number must contain the letter L followed by 8 digits")]
         public string LNumber { get; set; }
 
         public int MajorID { get; set; }
