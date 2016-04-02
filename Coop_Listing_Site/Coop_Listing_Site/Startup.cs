9955121a1259d6e38a20accd45c6dd5e83d65d86
Kethsar <Kethprall@gmail.com>
2016-04-02 15:02:11 -0700

    Start Identity integration

---
+++ Coop_Listing_Site/Coop_Listing_Site/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin;
+
+namespace Coop_Listing_Site
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Login") // Might be changed later
+            });
+        }
+    }
+}
