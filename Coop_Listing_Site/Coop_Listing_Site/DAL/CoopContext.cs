773b5add192be230edf661e6a9cdd400bf356c81
Chef-Code <lonnieteter@yahoo.com>
2016-04-02 19:01:38 -0700

    DbContext, Some Folders, SOC

---
+++ Coop_Listing_Site/Coop_Listing_Site/DAL/CoopContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Coop_Listing_Site.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Validation;
+using System.Text;
+
+namespace Coop_Listing_Site.DAL
+{
+    public class CoopContext : IdentityDbContext<User>
+    {
+        public CoopContext() : base("CoopContext")
+        {
+            //CoopContext Constructor
+        }
+
+        public DbSet<Opportunity> Opportunities { get; set; }
+        public DbSet<Major> Majors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
