b379ebb7067ee0e665777df877621cde27fbdedf
Tony Plueard <ptony25@gmail.com>
2016-04-04 15:32:31 -0700

    Added Login Information

--- Coop_Listing_Site/Coop_Listing_Site/Controllers/LoginController.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Controllers/LoginController.cs
@@ -1,5 +1,11 @@
-﻿using Coop_Listing_Site.Models.ViewModels;
+﻿using System.Web;
+using Coop_Listing_Site.Models.ViewModels;
+using Microsoft.AspNet.Identity;
 using System.Web.Mvc;
+using Coop_Listing_Site.DAL;
+using Coop_Listing_Site.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
 namespace Coop_Listing_Site.Controllers
 {
@@ -7,7 +13,8 @@ namespace Coop_Listing_Site.Controllers
     public class LoginController : Controller
     {
         // TODO: Proper code to let the user log in; after View Models and a dummy database are created
-
+        UserManager<User>userManager = new UserManager<User>(
+            new UserStore<User>(new CoopContext()));
         // GET: Login
         public ActionResult Index()
         {
@@ -24,6 +31,23 @@ namespace Coop_Listing_Site.Controllers
         [HttpPost]
         public ActionResult Student(StudentLoginModel student)
         {
+            if (!ModelState.IsValid) return View();
+            var user = userManager.Find(student.LNumber, student.Password);
+            if (user != null)
+            {
+                var identity = userManager.CreateIdentity(
+                    user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                GetAuthenticationManager().SignIn(identity);
+                
+
+                //TODO: Need to add a redirect after login 
+            }
+            
+
+            // user authentication failed
+            ModelState.AddModelError("", "Invalid email or password");
+            return View();
             return View();
         }
 
@@ -38,5 +62,22 @@ namespace Coop_Listing_Site.Controllers
         {
             return View();
         }
+
+        //Added Signin Method for the user
+        private void SignIn(User user)
+        {
+            var identity = userManager.CreateIdentity(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+
+            GetAuthenticationManager().SignIn(identity);
+        }
+
+        //Added authenticationMangager
+        private IAuthenticationManager GetAuthenticationManager()
+        {
+            var ctx = Request.GetOwinContext();
+            return ctx.Authentication;
+        }
     }
 }
+ 
--- Coop_Listing_Site/Coop_Listing_Site/Models/Company.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Company.cs
@@ -6,28 +6,10 @@ namespace Coop_Listing_Site.Models
     {
         // Inherits from User, doesn't need its own ID
         // public int CompanyID { get; set; }
-
-        [Required]
-        [Display(Name = "Company Name")]
         public string CompanyName { get; set; }
-
-        [Required]
-        [Display(Name = "City")]
         public string City { get; set; }
-
-        [Required]
-        [StringLength(2, ErrorMessage = "Enter the state abbreviation")]
-        [Display(Name = "State")]
         public string State { get; set; }
-
-        [StringLength(5, ErrorMessage = "The Zip Code should be 5 digits long.")]
-        [DataType(DataType.PostalCode, ErrorMessage = "Please Enter a valid ZipCode")]
-        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
-
-        [Required]
-        [DataType(DataType.Url, ErrorMessage = "Please Enter a valid URL")]
-        [Display(Name = "Company Website")]
         public string Website { get; set; }
 
     }
