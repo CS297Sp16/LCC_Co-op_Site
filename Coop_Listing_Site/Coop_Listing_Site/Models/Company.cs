e12747bc029d43c187b4d78007cc83c799697a8f
Chef-Code <lonnieteter@yahoo.com>
2016-04-06 18:50:35 -0700

    Populated all entities not involved with the User Model.  The User, Student, Coord, Company... need to be populated using the UserManager, I can do this in the next pass

--- Coop_Listing_Site/Coop_Listing_Site/Models/Company.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Company.cs
@@ -4,30 +4,11 @@ namespace Coop_Listing_Site.Models
 {
     public class Company : User
     {
-        // Inherits from User, doesn't need its own ID
-        // public int CompanyID { get; set; }
-
-        [Required]
-        [Display(Name = "Company Name")]  //This stuff is specific the User Interface and should be placed on the CompanyViewModel
         public string CompanyName { get; set; }
-
-        [Required]
-        [Display(Name = "City")]          //UI Specific
+        public string Address { get; set; }
         public string City { get; set; }
-
-        [Required]
-        [StringLength(2, ErrorMessage = "Enter the state abbreviation")]
-        [Display(Name = "State")]          //UI Specific 
         public string State { get; set; }
-
-        [StringLength(5, ErrorMessage = "The Zip Code should be 5 digits long.")]
-        [DataType(DataType.PostalCode, ErrorMessage = "Please Enter a valid ZipCode")]
-        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
-
-        [Required]
-        [DataType(DataType.Url, ErrorMessage = "Please Enter a valid URL")]
-        [Display(Name = "Company Website")]
         public string Website { get; set; }
 
     }
