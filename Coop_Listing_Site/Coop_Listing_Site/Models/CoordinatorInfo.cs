d349c1dec292bf38c80e97b06e9a503e32a99733
Kethsar <Kethprall@gmail.com>
2016-04-09 14:36:34 -0700

    Add proper foreign key to CoordinatorInfo, set Course collection in StudentInfo to use virtual keyword. Also might have forgotten to actually add files in last commit

---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/CoordinatorInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class CoordinatorInfo
+    {
+        public string LNumber { get; set; }
+        public int DepartmentID { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/StudentInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class StudentInfo
+    {
+        public string LNumber { get; set; }
+        public string UserId { get; set; }
+        public int MajorID { get; set; }
+        public virtual ICollection<Course> CompletedCourses { get; set; }
+    }
+}
