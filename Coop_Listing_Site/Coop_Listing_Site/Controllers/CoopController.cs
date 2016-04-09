5b340291927b93308b6239df98b0d4ead0b6e215
Kethsar <Kethprall@gmail.com>
2016-04-08 19:45:46 -0700

    Add base Coop and ControlPanel controllers

---
+++ Coop_Listing_Site/Coop_Listing_Site/Controllers/CoopController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coop_Listing_Site.Controllers
+{
+    public class CoopController : Controller
+    {
+        // This controller will have List, Details, and possibly Create, Delete, and Edit for all co-op opportunities
+
+        // GET: Coop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
