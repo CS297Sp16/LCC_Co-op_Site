660c79e2ad2188e05113c98c37ede99f271188d3
Tony Plueard <ptony25@gmail.com>
2016-04-11 15:29:17 -0700

    modified global.asax

--- Coop_Listing_Site/Coop_Listing_Site/Models/Major.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Major.cs
@@ -6,8 +6,12 @@ namespace Coop_Listing_Site.Models
     public class Major
     {
         public int MajorID { get; set; }
+        public int DepartmentID { get; set; }
         public string MajorName { get; set; }       //Required was set in the configuration class
-        public virtual ICollection<Course> Courses { get; set; }
+
+        //Will re-implement later on. Will be a feature to add towards the end after everything else is working the 
+        //way we want it to.
+        //public virtual ICollection<Course> Courses { get; set; }
 
     }
 }
