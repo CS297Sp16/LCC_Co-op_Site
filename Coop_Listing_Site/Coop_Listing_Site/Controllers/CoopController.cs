a6d674b8be3ef6f6aff003b7bc398525de09db8c
gafler <griffin.gabe@gmail.com>
2016-04-17 20:31:55 -0700

    updated error messages for adding opportunities
    updated error messages for adding opportunities.
    added buttons and return links to the add, edit, and delete opportunity
    views.
    commented out the need for a role on add and edit opportunities for
    testing, and added departmentID to add and edit methods

--- Coop_Listing_Site/Coop_Listing_Site/Controllers/CoopController.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Controllers/CoopController.cs
@@ -55,7 +55,7 @@ namespace Coop_Listing_Site.Controllers
         }
 
         //GET: CoopController/AddOpportunity
-        [Authorize(Roles = "Coordinator")]
+        //[Authorize(Roles = "Coordinator")]
         public ActionResult AddOpportunity()
         {
             //not sure if I should have a viewbag with users, and one for opportunities here
@@ -90,7 +90,8 @@ namespace Coop_Listing_Site.Controllers
                     Paid = opportunityVM.Paid,
                     Duration = opportunityVM.Duration,
                     OpeningsAvailable = opportunityVM.OpeningsAvailable,
-                    TermAvailable = opportunityVM.TermAvailable
+                    TermAvailable = opportunityVM.TermAvailable,
+                    DepartmentID = opportunityVM.DepartmentID
                 };
                 db.Opportunities.Add(opportunity);
                 db.SaveChanges();
@@ -101,7 +102,7 @@ namespace Coop_Listing_Site.Controllers
         }
 
         //GET: CoopController/EditOpportunity
-        [Authorize(Roles = "Coordinator")]
+        //[Authorize(Roles = "Coordinator")]
         public ActionResult EditOpportunity(int? id)
         {
             if (id == null)
