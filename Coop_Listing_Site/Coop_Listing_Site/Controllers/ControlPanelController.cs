ed6690cf9c4279812e3a16c75d71d069b2e9e078
Chef-Code <lonnieteter@yahoo.com>
2016-04-25 12:17:55 -0700

    revert2

--- Coop_Listing_Site/Coop_Listing_Site/Controllers/ControlPanelController.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Controllers/ControlPanelController.cs
@@ -12,6 +12,7 @@ using Coop_Listing_Site.Models.ViewModels;
 using System.Diagnostics;
 using System.Data.Entity;
 
+<<<<<<< HEAD
 namespace Coop_Listing_Site.Controllers
 {
     public class ControlPanelController : Controller
@@ -88,6 +89,84 @@ namespace Coop_Listing_Site.Controllers
             ViewBag.ReturnMessage = invitation.SendInvite();
 
             return View();
+=======
+namespace Coop_Listing_Site.Controllers
+{
+    public class ControlPanelController : Controller, IControlPanelRepository
+    {
+        // Might need a rename. This will have a Student and Advisor action result to start, which will point to their views.
+        // GET: ControlPanel
+        private CoopContext db;
+        private UserManager<User> userManager;
+
+        public ControlPanelController()
+        {
+            db = new CoopContext();
+            userManager = new UserManager<User>(new UserStore<User>(db));
+        }
+
+        //[Authorize]
+        public ActionResult Index()
+        {
+            var currentUser = CurrentUser;
+            return View(currentUser);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult SMTP()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin"), HttpPost]
+        public ActionResult SMTP(string SMTPAddress, string SMTPUser, string SMTPPassword, string InviteEmail, string Domain)
+        {
+            EmailInfo.SMTPAddress = SMTPAddress;
+            EmailInfo.SMTPAccountName = SMTPUser;
+            EmailInfo.SMTPPassword = SMTPPassword;
+            EmailInfo.InviteEmail = InviteEmail;
+            EmailInfo.Domain = Regex.Replace(Domain, "^https?://", "", RegexOptions.IgnoreCase);
+
+            return View();
+        }
+
+        [Authorize(Roles = "Coordinator")]
+        public ActionResult Invite()
+        {
+            ViewBag.SMTPReady = EmailInfo.ProperlySet;
+
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        [Authorize(Roles = "Coordinator")]
+        public ActionResult Invite([Bind(Include = "Email,UserType")] RegisterInvite invitation)
+        {
+            ViewBag.SMTPReady = EmailInfo.ProperlySet;
+            if (!ModelState.IsValid) return View();
+
+            var email = db.Invites.FirstOrDefault(i => i.Email.ToLower() == invitation.Email.ToLower());
+            if (email != null)
+            {
+                ModelState.AddModelError("Email", "An invitation has already been sent to that e-mail!");
+                return View();
+            }
+
+            var user = userManager.FindByEmail(invitation.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError("Email", "User with that e-mail already exists!");
+                return View();
+            }
+
+            invitation.RegisterInviteID = Guid.NewGuid().ToString("N");
+            db.Invites.Add(invitation);
+            db.SaveChanges();
+
+            ViewBag.ReturnMessage = invitation.SendInvite();
+
+            return View();
+>>>>>>> parent of 0216dd2... forgot to take out the interface in the controller
         }
 
 <<<<<<< HEAD
