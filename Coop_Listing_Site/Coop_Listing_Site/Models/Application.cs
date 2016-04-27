055798164165f69c5ee80184cc94916c5c487e3f
Tony Plueard <ptony25@gmail.com>
2016-04-27 14:12:27 -0700

    Added restriction to file types that can be uploaded

--- Coop_Listing_Site/Coop_Listing_Site/Models/Application.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Application.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+//using System.ComponentModel.DataAnnotations;
 
 namespace Coop_Listing_Site.Models
 {
@@ -10,7 +11,7 @@ namespace Coop_Listing_Site.Models
         public int ApplicationId { get; set; }
         public int UserId { get; set; }
         public int OpportunityId { get; set; }
-        //This is where the resumes will be stored in the DB
+        //This is where the resumes will be stored in the DB     
         public byte[] Resume { get; set; }
         //This is where the cover letters will be stored in the DB
         public byte[] CoverLetter { get; set; }
