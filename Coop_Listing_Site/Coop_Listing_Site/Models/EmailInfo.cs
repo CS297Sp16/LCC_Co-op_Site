acaed741f522b55c282ce3efe6c583db97f4b59e
Kethsar <Kethprall@gmail.com>
2016-05-18 09:48:39 -0700

    Fix an issue in EmailInfo using the wrong word for the invite

--- Coop_Listing_Site/Coop_Listing_Site/Models/EmailInfo.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/EmailInfo.cs
@@ -59,7 +59,7 @@ namespace Coop_Listing_Site.Models
 
                         using (var mail = new MailMessage(fromEmail, invitation.Email))
                         {
-                            string message = "You have been invited to use the Lane Community College Co-op Listing website as a {3}.{0}" +
+                            string message = "You have been invited to use the Lane Community College Co-op Listing website as a {1}.{0}" +
                                                 "To register, please click the link below. You will be taken to the registration page, with your e-mail already filled out for you.{0}" +
                                                 "https://{3}/Register/{1}/{2} {0}{0}" +
                                                 "This is an automatic message. Any replies sent to this e-mail will not be viewed.";
