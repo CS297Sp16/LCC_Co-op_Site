1c7daef6a1ad8c4f9560703be00fe36099199ac9
Kethsar <Kethprall@gmail.com>
2016-05-22 17:41:21 -0700

    Add UserFile class to be used with files needed to be uploaded for an application

---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/UserFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class UserFile
+    {
+        public int UserFileID { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string FileName { get; set; }
+
+        public byte[] FileData { get; set; }
+    }
+}
