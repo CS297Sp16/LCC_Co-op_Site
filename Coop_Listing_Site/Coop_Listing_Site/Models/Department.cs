8a55a36eac44c80a718eb775813fde934bdc5623
gafler <griffin.gabe@gmail.com>
2016-05-25 12:29:03 -0700

    fixing merge issues

--- Coop_Listing_Site/Coop_Listing_Site/Models/Department.cs
+++ Coop_Listing_Site/Coop_Listing_Site/Models/Department.cs
@@ -10,9 +10,7 @@ namespace Coop_Listing_Site.Models
             // make sure Majors isn't null when we make a new Department
             Majors = new List<Major>();
         }
-        [Display(Name ="Department ID")]
         public int DepartmentID { get; set; }
-        [Required(ErrorMessage = "Please enter a department name"), Display(Name = "Department name")]
         public string DepartmentName { get; set; }
 
         public virtual ICollection<Major> Majors { get; set; }
