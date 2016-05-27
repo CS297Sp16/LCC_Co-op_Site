2c032e234056bfb25f237752d479cb829c60bbed
Chef-Code <lonnieteter@yahoo.com>
2016-05-26 18:17:41 -0700

    Added an AgreementForm Model, which is a required form or process by the student.  Dosen't break anything

---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/WeeklySchedule.cs
@@ -0,0 +1,17 @@
+﻿using Coop_Listing_Site.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class WeeklySchedule
+    {
+        WeeklySchedule()
+        {
+            WorkDays = new HashSet<WorkDay>();
+        }
+        public ICollection<WorkDay> WorkDays { get; set; }
+    }
+}
---
+++ Coop_Listing_Site/Coop_Listing_Site/Models/WorkDay.cs
@@ -0,0 +1,15 @@
+﻿using Coop_Listing_Site.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coop_Listing_Site.Models
+{
+    public class WorkDay
+    {
+        public WeekDay WeekDay { get; set; }
+        public TimeSpan Arrival { get; set; }
+        public TimeSpan Departure { get; set; }
+    }
+}
